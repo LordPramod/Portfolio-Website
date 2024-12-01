2ec7a4de59b49785391688656d150c605daabdd0
LordPramod <thapapramod821@gmail.com>
2024-12-01 11:47:01 +0545

    typescript removed

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// var typed = new Typed(".typing-2", {
+//     strings: ["Developer", "Gamer","Blogger", "Designer", "Freelancer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
